bc597ced4bde8ae73931bf9bcc35b196b19f50c3
Savanh <2217905486@qq.com>
2021-05-04 21:50:57 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/about', 'StaticPagesController@about');
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('/signup','UsersController@create')->name('signup');
